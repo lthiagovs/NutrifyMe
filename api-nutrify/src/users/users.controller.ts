d6d8d0d6e0f69fea40480c8cc23e2b8bb3ab83ff
lthiagovs <lthiagofarias@gmail.com>
2024-11-22 22:24:37 -0300

    :sparkles: feat(api): users controller endpoints placeholder

--- api-nutrify/src/users/users.controller.ts
+++ api-nutrify/src/users/users.controller.ts
@@ -1,8 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
 
-    
+    @Get(':id')
+    async getUserByID(@Param('id') id: string): Promise<any>{
+        return 1
+    }
+
+    @Post()
+    async createUser(@Body() user: any): Promise<any>{
+
+    }
+
+    @Put(':id')
+    async updateUser(@Param('id') id: string, @Body() user: any): Promise<any>{
+        
+    }
+
+    @Delete(':id')
+    async deleteUser(@Param('id') id: string): Promise<any>{
+        
+    }
 
 }
