e50e839da5d87cc64d44db59a7aa563a7e61af5e
lthiagovs <lthiagofarias@gmail.com>
2024-11-22 22:14:12 -0300

    :sparkles: feat(api) add services, controllers and modules (user and consultation)

--- api-nutrify/src/app.module.ts
+++ api-nutrify/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ConsultationModule } from './consultation/consultation.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ConsultationModule],
   controllers: [AppController],
   providers: [AppService],
 })
