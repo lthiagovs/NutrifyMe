e3b49fd2216e115090935fa46ab8513570ee6793
lthiagovs <lthiagofarias@gmail.com>
2024-11-22 23:29:46 -0300

    :sparkles: feat(api) mongoose connection

--- api-nutrify/src/app.module.ts
+++ api-nutrify/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConsultationModule } from './consultation/consultation.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, ConsultationModule],
+  imports: [UsersModule, ConsultationModule, MongooseModule.forRoot('mongodb://localhost:27017/')],
   controllers: [AppController],
   providers: [AppService],
 })
