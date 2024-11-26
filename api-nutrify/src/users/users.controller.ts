6422ca9f2e1efb3b9d383e7ff2f23b4dd1d22857
lthiagovs <lthiagofarias@gmail.com>
2024-11-25 23:22:16 -0300

    :sparkles: feat(user): implement basic CRUD controller

--- api-nutrify/src/users/users.controller.ts
+++ api-nutrify/src/users/users.controller.ts
@@ -1,26 +1,31 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { User } from './user/user';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
 
+    constructor(private readonly userService: UsersService){}
+
+
     @Get()
-    async getAllUsers(@Param('id') id: string): Promise<any>{
-        return 1
+    async getAllUsers(): Promise<User[]>{
+        return this.userService.listAllUsers();
     }
 
     @Post()
-    async createUser(@Body() user: any): Promise<any>{
-
+    async createUser(@Body() user: User): Promise<User>{
+        return this.userService.createUser(user);
     }
 
     @Put(':id')
-    async updateUser(@Param('id') id: string, @Body() user: any): Promise<any>{
-        
+    async updateUser(@Param('id') id: string, @Body() user: User): Promise<User>{
+        return this.userService.updateUser(id,user);
     }
 
     @Delete(':id')
-    async deleteUser(@Param('id') id: string): Promise<any>{
-        
+    async deleteUser(@Param('id') id: string): Promise<User>{
+        return this.userService.removeUser(id);
     }
 
 }
