1b755ed618e6d78e397f6e6b783ea3f26270dacf
lthiagovs <lthiagofarias@gmail.com>
2024-12-02 23:19:52 -0300

    :sparkles: feat(api) docker container created

--- api-nutrify/src/app.module.ts
+++ api-nutrify/src/app.module.ts
@@ -2,26 +2,34 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
 import { ConsultationsModule } from './consultations/consultations.module';
 import { GuardService } from './guard/guard.service';
 import { AuthService } from './auth/auth.service';
 import { JwtModule, JwtService } from '@nestjs/jwt';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import Configuration from './config/Configuration';
 
 @Module({
   imports: [
-    ConfigModule.forRoot( {isGlobal: true, envFilePath: '.env'} ),  // Carrega o arquivo .env
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }), // Globalmente disponível
     UsersModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule], // Importa o ConfigModule para uso local
+      inject: [ConfigService], // Garante que o ConfigService seja injetado
+      useFactory: (configService: ConfigService): MongooseModuleOptions => {
+        return {
+          uri: configService.get<string>('DATABASE_URL'), // Certifique-se de que 'database.url' está definido no .env
+        };
+      },
+    }),
     ConsultationsModule,
     JwtModule.register({
-      secret: process.env.JWT_SECRET || 'nutrifyme', // Certifique-se de definir essa chave
-      signOptions: { expiresIn: '60s' }, // Opcional: ajuste o tempo de expiração
+      secret: process.env.JWT_SECRET || 'nutrifyme', // Define a chave secreta para JWT
+      signOptions: { expiresIn: '60s' }, // Ajusta o tempo de expiração
     }),
   ],
   controllers: [AppController],
   providers: [AppService, GuardService, AuthService, JwtService],
-  
 })
 export class AppModule {}
